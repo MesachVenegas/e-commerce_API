0b79fe12441e0a841b0d42fdf2cf13c1cdc60e58
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-31 10:17:23 -0700

    create: generate sequelize-cli  models

---
+++ src/migrations/20230531171446-create-orders.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Orders', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            totalPrice: {
+                type: Sequelize.REAL
+            },
+            userId: {
+                type: Sequelize.INTEGER
+            },
+            isCompleted: {
+                type: Sequelize.BOOLEAN
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Orders');
+    }
+};
