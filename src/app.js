13502e60c0a6816337c2bd638a7cc920d8f5df90
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-03 16:00:34 -0700

    add: routes api and error handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const apiRouter = require('./routes')
+const errorRouter = require('./routes/errorRoutes');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+
+
+apiRouter(app);
+errorRouter(app);
 
 app.listen(PORT, () => {
     console.log(`Servidor en linea y escuchando en el puerto  ${PORT}`)
