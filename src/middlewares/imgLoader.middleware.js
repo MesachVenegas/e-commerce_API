feead2e34a1eab6e604a5a24b2d27c5c52af00d9
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-04 00:12:23 -0700

    create: image loader middleware with multer

---
+++ src/middlewares/imgLoader.middleware.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const types = ['image/png', 'image/jpeg', 'image/webp']
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: './src/filesUpload',
+        filename: (req, file, cb) => {
+            const date = Date.now();
+            cb(null, `${date}_${file.originalname}`);
+        }
+    }),
+    limits: {
+        fileSize: 600000, // kilobytes(kb)
+    },
+    fileFilter: (req, file, cb) => {
+        if (!types.includes(file.mimetype)) {
+            cb({
+                status: 400,
+                name: 'File type unsupported',
+                message: `Invalid type, only allowed ${types.join(', ')} mimetypes`,
+            }, false)
+        } else {
+            cb(null, true);
+        }
+    },
+}).single('avatar');
+
+module.exports = upload;
