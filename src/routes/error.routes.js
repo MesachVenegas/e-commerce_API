33bb1c78b1c6a7b86955ccd566bccdf808e92bde
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-03 19:11:01 -0700

    typo: rename errorRoutes to error.routes

--- src/routes/error.routes.js
+++ src/routes/error.routes.js
@@ -6,6 +6,12 @@ const errorRoutes = (app) => {
     app.use(errorLogger);
     app.use(errorHandler);
     app.use(ormErrorHandler);
+
+    app.use('*', (req, res) => {
+        res.status(404).json({
+            message: "Sorry we couldn't find them"
+        });
+    })
 }
 
 module.exports = errorRoutes;
