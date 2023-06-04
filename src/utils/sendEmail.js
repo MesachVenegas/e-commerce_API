72e5ec369995adc1063ed590ca5f95d341fc96b2
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-04 09:03:47 -0700

    create: email sender

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,59 @@
+const transporter = require('./transporter');
+const path = require('path');
+const ejs = require('ejs');
+
+const sendNewEmail = (email, doc, attachments, subject) => {
+    transporter.sendMail({
+        from: 'mesachman@gmail.com',
+        to: email,
+        subject: subject,
+        text: 'Welcome to E-commerce Services, the most popular e-commerce service',
+        html: doc, attachments,
+    })
+        .then(() => console.log(`A email was sent`))
+        .catch(err => console.error(err));
+}
+
+const sendWelcomEmail = async (email, data) => {
+    const filePath = path.join(__dirname, '../views/welcome.ejs');
+    const doc = await ejs.renderFile(filePath, data);
+    const subject = "Welcome to E-commerce Services";
+    const attachments = [
+        {
+            filename: "illo_welcome_1.png",
+            path: path.join(__dirname, "../views/images/illo_welcome_1.png"),
+            cid: "welcome_image",
+        },
+        {
+            filename: "Logo.png",
+            path: path.join(__dirname, "../views/images/Logo.png"),
+            cid: "logo",
+        },
+        {
+            filename: "facebook2x.png",
+            path: path.join(__dirname, "../views/images/facebook2x.png"),
+            cid: "facebook",
+        },
+        {
+            filename: "twitter2x.png",
+            path: path.join(__dirname, "../views/images/twitter2x.png"),
+            cid: "twitter",
+        },
+        {
+            filename: "instagram2x.png",
+            path: path.join(__dirname, "../views/images/instagram2x.png"),
+            cid: "instagram",
+        },
+        {
+            filename: "pinterest2x.png",
+            path: path.join(__dirname, "../views/images/pinterest2x.png"),
+            cid: "pinterest",
+        },
+    ];
+
+    sendNewEmail(email, doc, attachments, subject);
+}
+
+module.exports = {
+    sendWelcomEmail
+};
