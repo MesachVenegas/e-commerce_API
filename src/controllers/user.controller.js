dc21222fb44cde09c584cd759a8cb59606226a38
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-03 19:10:39 -0700

    create: user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,13 +4,45 @@ const createUser = async (req, res, next) => {
     try {
         const data = req.body;
         await UserService.registeredUser(data);
-        res.status(200).send();
+        res.status(201).send();
     } catch (error) {
         next(error);
     }
 }
 
+const getUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await UserService.searchUser(id);
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const userDelete = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await UserService.deleteUser(id);
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+}
+
+const logIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const data = await UserService.userLogin({ reqEmail: email, reqPassword: password });
+        res.status(200).json(data);
+    } catch (error) {
+        next(error);
+    }
+}
 
 module.exports = {
-    createUser
+    createUser,
+    getUser,
+    userDelete,
+    logIn
 };
