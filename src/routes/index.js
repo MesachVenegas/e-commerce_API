4fe43d645080ef23987d90aba586a1666344f0d6
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-06 15:33:47 -0700

    add: products routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const userRoutes = require('./user.routes');
+const productsRoutes = require('./products.routes');
 
 const apiRouter = (app) => {
     app.use(userRoutes);
+    app.use(productsRoutes);
 }
 
 module.exports = apiRouter;
