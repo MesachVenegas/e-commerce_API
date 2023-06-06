51f8f8145bf1ed11b22df5a0290b813346afbc76
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-06 15:35:48 -0700

    create: user seed

---
+++ src/seeders/20230606155030-demo-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        username: 'Mesach Venegas',
+        email: 'mesach.venegas@gmail.com',
+        password: '123456',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'John Doe',
+        email: 'john.doe@gmail.com',
+        password: '123456',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Ian Rosas',
+        email: 'iannacus@gmail.com',
+        password: '123456',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
