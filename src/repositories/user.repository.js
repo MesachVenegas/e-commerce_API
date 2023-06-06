62bfa69649a8f8243e652ca82bc16b7f14bf1aef
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-06 15:33:24 -0700

    change: model namo users to Users

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,12 +1,12 @@
-const { users } = require('../models');
+const { Users } = require('../models');
 
 const insertUser = async (newUser) => {
-    const user = await users.create(newUser);
+    const user = await Users.create(newUser);
     return user;
 }
 
 const findUser = async (id) => {
-    const user = await users.findByPk(id, {
+    const user = await Users.findByPk(id, {
         attributes: {
             exclude: ['password'],
         }
@@ -15,27 +15,27 @@ const findUser = async (id) => {
 }
 
 const updateUsername = async (id, newUserName) => {
-    const user = await users.update(newUserName, { where: { id } });
+    const user = await Users.update(newUserName, { where: { id } });
     return user;
 }
 
 const destroyUser = async (id) => {
-    const user = await users.destroy({ where: { id } });
+    const user = await Users.destroy({ where: { id } });
     return user;
 }
 
 const login = async (email) => {
-    const user = await users.findOne({ where: { email } });
+    const user = await Users.findOne({ where: { email } });
     return user;
 }
 
 const setAvatarUrl = async (id, url) => {
-    const user = await users.update(url, { where: { id: id } });
+    const user = await Users.update(url, { where: { id: id } });
     return user;
 }
 
 const recoveryPassword = async (id, newPass) => {
-    const user = await users.update(newPass, { where: id });
+    const user = await Users.update(newPass, { where: id });
     return user;
 }
 
