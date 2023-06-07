1779ce723bdf8a6d62937cab97f8576804f24608
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-07 04:52:37 -0700

    add: routes to load images and edit product data

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,14 @@
 const { uploadProductImgs} = require('../utils/imgLoader');
-const { newItemData, getProducts, loadThumbail } = require('../controllers/products.controller');
+const { newItemData, getProducts, loadThumbail, editProducts } = require('../controllers/products.controller');
 const { Router } = require('express')
 
 const router = Router();
 
 router.post('/products', newItemData);
 
-router.post('/products/:id/images', uploadProductImgs, loadThumbail)
+router.put('/products/:id/images', uploadProductImgs, loadThumbail)
+
+router.put('/products/:id', editProducts);
 
 router.get('/products', getProducts);
 
