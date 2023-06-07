5b80120d7240b8ba1132e85f564eb516fef1cb33
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-06 20:18:58 -0700

    add: swagger documentation route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
-const userRoutes = require('./user.routes');
 const productsRoutes = require('./products.routes');
+const SwaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json')
+const userRoutes = require('./user.routes');
 
 const apiRouter = (app) => {
     app.use(userRoutes);
     app.use(productsRoutes);
+    // ? Documentation
+    app.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDoc));
 }
 
 module.exports = apiRouter;
