0fdf05cb38d4c35bc10884e4f90a8ccc74997ec4
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-07 03:56:49 -0700

    create: service to load and set url to products

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,4 +1,4 @@
-const { addNewProduct, getAllProductsInStock } = require('../repositories/products.repository');
+const { addNewProduct, getAllProductsInStock, insertImagesUrl } = require('../repositories/products.repository');
 
 class ProductServices {
     static async addProduct(data) {
@@ -13,6 +13,20 @@ class ProductServices {
     static async getAllProducts() {
         try {
             const response = await getAllProductsInStock();
+            return response;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async loadImages(id, images) {
+        try {
+            if (images.length <= 0) {
+                throw new Error('we couldn\'t find images')
+            }
+            const urls = images.map(image => image.path)
+            console.log(urls)
+            const response = await insertImagesUrl(id, urls);
             return response;
         } catch (error) {
             throw error;
