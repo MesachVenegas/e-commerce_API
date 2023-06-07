87f3b05b1ed6fc83d53ba15e3cba7da0f80d3ef8
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-07 03:57:03 -0700

    add: route to load product images

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,12 @@
-const { newItemData, getProducts } = require('../controllers/products.controller');
+const { uploadProductImgs} = require('../utils/imgLoader');
+const { newItemData, getProducts, loadThumbail } = require('../controllers/products.controller');
 const { Router } = require('express')
 
 const router = Router();
 
 router.post('/products', newItemData);
+
+router.post('/products/:id/images', uploadProductImgs, loadThumbail)
 
 router.get('/products', getProducts);
 
