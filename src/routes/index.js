1468df17ebd8c9f354eba9707fcf19461d695c3a
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-09 05:38:34 -0700

    add: cart routes to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const productsRoutes = require('./products.routes');
 const SwaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('../swagger.json')
+const swaggerDoc = require('../swagger.json');
+const cartRoutes = require('./carts.routes');
 const userRoutes = require('./user.routes');
 
 const apiRouter = (app) => {
     app.use(userRoutes);
     app.use(productsRoutes);
+    app.use(cartRoutes);
+
     // ? Documentation
     app.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDoc));
 }
