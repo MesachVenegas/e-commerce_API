6d4baf4252b7857a4b6a288cd1103aa40aebcdb2
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-09 05:38:25 -0700

    create: cart routes

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -3,10 +3,10 @@ const { Router } = require('express')
 
 const router = Router();
 
-router.post('/carts/create', createCart);
-
-router.put('/carts/update/:id', updateCart);
-
 router.get('/carts/:id', getCart);
+
+router.post('/carts/create/:userId', createCart);
+
+router.put('/carts/updated/:id', updateCart);
 
 module.exports = router
