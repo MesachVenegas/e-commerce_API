b37995c99080a864dbd1c59838ff3aeaa2d7c253
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-10 01:03:42 -0700

    changes: removed time stamps

--- src/migrations/20230606163830-create-products.js
+++ src/migrations/20230606163830-create-products.js
@@ -4,59 +4,53 @@ const { DataTypes } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Products', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      description: {
-        allowNull: false,
-        type: Sequelize.TEXT
-      },
-      price: {
-        allowNull: false,
-        type: Sequelize.REAL
-      },
-      stock: {
-        defaultValue: 1,
-        type: Sequelize.INTEGER
-      },
-      available: {
-        defaultValue: true,
-        type: Sequelize.BOOLEAN
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
-        references: {
-          model: "Users",
-          key: "id"
-        },
-        field: "user_id"
-      },
-      img: {
-        type: Sequelize.ARRAY(DataTypes.STRING)
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Products');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Products', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            description: {
+                allowNull: false,
+                type: Sequelize.TEXT
+            },
+            price: {
+                allowNull: false,
+                type: Sequelize.REAL
+            },
+            stock: {
+                defaultValue: 1,
+                type: Sequelize.INTEGER
+            },
+            available: {
+                defaultValue: true,
+                type: Sequelize.BOOLEAN
+            },
+            userId: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                onDelete: "CASCADE",
+                onUpdate: "CASCADE",
+                references: {
+                    model: "Users",
+                    key: "id"
+                },
+                field: "user_id"
+            },
+            img: {
+                type: Sequelize.ARRAY(DataTypes.STRING)
+            },
+        }, {
+            timestamps: false,
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Products');
+    }
 };
--- src/models/products.js
+++ src/models/products.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     }, {
         sequelize,
         modelName: 'Products',
+        timestamps: false
     });
     return Products;
 };
