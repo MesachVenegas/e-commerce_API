37c7741515f7a98e986197f97250132f8ab09e7b
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-10 18:47:13 -0700

    style: change spaces and identation

--- src/models/carts.js
+++ src/models/carts.js
@@ -6,11 +6,18 @@ module.exports = (sequelize, DataTypes) => {
     class Carts extends Model {
         static associate(models) {
             // Un carrito le pertenece a un usuarios.
-            Carts.belongsTo(models.Users, { foreignKey: "userId", targetKey: "id" })
+            Carts.belongsTo(models.Users,{
+                foreignKey: "userId",
+                targetKey: "id"
+            })
             // Un carrito tiene muchos productos
-            Carts.belongsToMany(models.Products, { through: "ProductInCarts", foreignKey: "cartId" })
+            Carts.belongsToMany(models.Products,{
+                through: "ProductInCarts",
+                foreignKey: "cartId"
+            })
         }
     }
+
     Carts.init({
         userId: { type: DataTypes.INTEGER, field: "user_id" },
         totalPrice: { type: DataTypes.REAL, field: "total_price" },
