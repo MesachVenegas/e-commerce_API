87189513b659441c5693a4df59f0b66b30ea3b4d
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-10 17:08:46 -0700

    changes: includes on cart query and  repositories to manages product on cart

--- src/repositories/cart.repository.js
+++ src/repositories/cart.repository.js
@@ -1,22 +1,65 @@
-const { Carts } = require('../models');
+const { Op } = require('sequelize');
+const { Carts, Users, Products, ProductInCarts } = require('../models');
 
 const getCart = async (id) => {
-    return await Carts.findAll({ where: { id: id}});
+    return await Carts.findAll({
+        where: { id: id },
+        attributes: { exclude: ['userId'] },
+        include: [
+            {
+                model: Users,
+                attributes: { exclude: ['password'] }
+            },
+            {
+                model: Products,
+                attributes: { exclude: ['userId']},
+                include: {
+                    model: Users
+                }
+            }
+        ]
+    });
+}
+
+const addProduct = async (product) => {
+    return await ProductInCarts.create(product);
 }
 
 const createUserCart = async (data) => {
-    console.log(data);
-    const cart = "await"
-    // const cart = await Carts.create(data);
-    return cart;
+    return await Carts.create(data);
+}
+
+const getProducts = async (id) => {
+    return await ProductInCarts.findAll({
+        where: {
+            [Op.and]: [
+                { cartId: id },
+                { status: false },
+            ],
+        },
+    });
 }
 
 const updateTotal = async (id, data) => {
     return await Carts.update(data, { where: { id } });
 }
 
+const getProductsInCart = async (cartId) => {
+    return await ProductInCarts.findAll({ where: { cartId } });
+}
+
+const updateQuantity = async (productId) => {
+    const product = await ProductInCarts.findByPk(productId);
+    await product.increment('quantity', { by: 1 });
+return product;
+}
+
 module.exports = {
     getCart,
+    getProducts,
     createUserCart,
-    updateTotal
+    updateTotal,
+    updateQuantity,
+    addProduct,
+    getProductsInCart
 };
