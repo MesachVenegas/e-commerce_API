5a2b13f2bfba9438419396480d6aea6c7573f848
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-12 03:55:17 -0700

    delete: route carts/:id/products

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,14 +1,16 @@
-const { createCart, updateCart, getCart, addProduct } = require('../controllers/carts.controller');
+const { createCart, updateCart, getCart, addProduct, prepareOrder } = require('../controllers/carts.controller');
 const { Router } = require('express')
 
 const router = Router();
 
 router.get('/carts/:id', getCart);
 
+router.get('/carts/:id/products', prepareOrder);
+
 router.post('/carts/create/:userId', createCart);
 
-router.put('/carts/updated/:id', updateCart);
+router.put('/carts/:id/products', addProduct);
 
-router.post('/carts/:id/products', addProduct);
+router.put('/carts/updated/:id', updateCart);
 
 module.exports = router
