c99be3b8588f4c556171ec63bc5d1e679891b4e0
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-13 08:12:56 -0700

    add: validators to Users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 const { createUser, getUser, userDelete, logIn, updateAvatar, updateUserName } = require('../controllers/user.controller');
-const { validateCreation } = require('../validators/user.validate');
+const { validateCreation, validateNewUsername } = require('../validators/user.validate');
+const tokenAuth = require('../middlewares/auth.middleware');
 const { uploadAvatar } = require('../utils/imgLoader');
 const { Router } = require('express')
 const router = Router();
 
-router.post('/users/:id/avatar', uploadAvatar, updateAvatar);
+router.post('/users/:id/avatar', tokenAuth, uploadAvatar, updateAvatar);
 
 router.post('/users', validateCreation, createUser);
 
@@ -12,8 +13,8 @@ router.post('/users/login', logIn);
 
 router.get('/users/:id', getUser);
 
-router.delete('/users/:id', userDelete);
+router.delete('/users/:id', tokenAuth, userDelete);
 
-router.put('/users/:id/profile', updateUserName);
+router.put('/users/:id/profile', tokenAuth, validateNewUsername, updateUserName);
 
 module.exports = router
