37f5347e73a2365b1b53df1e776bbe8ee8f9b49a
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-14 03:04:42 -0700

    add: service to complete order

--- src/services/orders.service.js
+++ src/services/orders.service.js
@@ -1,4 +1,6 @@
-const { initOrder, getUserOrders } = require('../repositories/orders.repository');
+const { initOrder, getUserOrders, completeProduct, findOrderById, completeOrder, getProductsInOrder } = require('../repositories/orders.repository');
+const { findUser } = require('../repositories/user.repository');
+const sendBoughtEmail = require('../utils/orderEmail');
 
 class OrderServices {
     static async createOrder(userId) {
@@ -14,6 +16,29 @@ class OrderServices {
         try {
             const orders = await getUserOrders(userId);
             return orders;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    static async completeOrder(orderId) {
+        try {
+            // Obtener los productos de la orden
+            const products = await getProductsInOrder(orderId);
+            // marcar los productos como comprados
+            for(const product of products) {
+                await completeProduct(orderId, product.productId);
+            }
+            // marcar la orden como completada
+            await completeOrder(orderId);
+            // enviar correo de confirmacion de orden de compra.
+            const order = await findOrderById(orderId);
+            const user = await findUser(order.userId);
+            sendBoughtEmail(user.email, {
+                username: user.username,
+                order: order
+            })
         } catch (error) {
             throw error;
         }
