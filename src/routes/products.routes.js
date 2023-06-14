8a7752ae94f02cf23047c49a0f0e768bd177aa0a
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-14 03:44:20 -0700

    add: validators on product routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,15 +1,16 @@
 const { newItemData, getProducts, loadThumbail, editProducts } = require('../controllers/products.controller');
+const { validateNewProduct, updateProduct } = require('../validators/product.validate');
 const tokenAuth = require('../middlewares/auth.middleware');
 const { uploadProductImgs} = require('../utils/imgLoader');
-const { Router } = require('express')
+const { Router } = require('express');
 
 const router = Router();
 
-router.post('/products', tokenAuth, newItemData);
+router.post('/products', tokenAuth, validateNewProduct, newItemData);
 
 router.put('/products/:id/images', tokenAuth, uploadProductImgs, loadThumbail)
 
-router.put('/products/:id', tokenAuth, editProducts);
+router.put('/products/:id', tokenAuth, updateProduct, editProducts);
 
 router.get('/products', getProducts);
 
