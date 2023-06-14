8f39fbdecfb883b1e56546b16b50e22c7b26bb53
MesachVenegas <mesach.venegas@hotmail.com>
2023-06-14 03:59:55 -0700

    fix: validations products

--- src/validators/product.validate.js
+++ src/validators/product.validate.js
@@ -6,24 +6,22 @@ const validateNewProduct = [
         .exists().withMessage('name field is required')
         .trim().notEmpty().withMessage('name field cannot be empty')
         .isString().withMessage('name field must be a string')
-        .isLength({ min: '10'}).withMessage('name field must be at least 10 characters')
-        .matches(/^[a-zA-Z0-9]+$/).withMessage('name field only contains letters and numbers'),
+        .isLength({ min: '10'}).withMessage('name field must be at least 10 characters'),
     check('description', 'error validating description field')
         .exists().withMessage('description field is required')
         .trim().notEmpty().withMessage('description field cannot be empty')
         .isString().withMessage('description field description must be a string')
-        .isLength({ min: '100'}).withMessage('description field must be at least 100 characters')
-        .matches(/^[a-zA-Z0-9!.,_-]+$/).withMessage('description fiel only must contains letters, numbers, punctuation symbols, and underscore or middle dash'),
+        .isLength({ min: '20'}).withMessage('description field must be at least 100 characters'),
     check('price', 'error validating price field')
         .exists().withMessage('price field is required')
         .trim().notEmpty().withMessage('price field cannot be empty')
-        .isNumeric().withMessage('price field must be a number')
-        .matches(/^[0-9]+$/).withMessage('price field only contains numbers'),
+        .isNumeric().withMessage('price field must be a number'),
     check('stock', 'error validating stock field')
         .trim().notEmpty().withMessage('stock field cannot be empty')
         .isNumeric().withMessage('stock field must be a number')
         .matches(/^[0-9]+$/).withMessage('stock field only contains numbers'),
     check('available', 'error validating available field')
+        .optional()
         .isBoolean().withMessage('available field must be true or false'),
     (req, res, next) => {
         fieldValidate(req, res, next);
@@ -32,24 +30,29 @@ const validateNewProduct = [
 
 const updateProduct = [
     check('name', 'error validating name field')
+        .optional()
         .trim().notEmpty().withMessage('name field cannot be empty')
         .isString().withMessage('name field must be a string')
         .isLength({ min: '10'}).withMessage('name field must be at least 10 characters')
         .matches(/^[a-zA-Z0-9]+$/).withMessage('name field only contains letters and numbers'),
     check('description', 'error validating description field')
+        .optional()
         .trim().notEmpty().withMessage('description field cannot be empty')
         .isString().withMessage('description field description must be a string')
         .isLength({ min: '100'}).withMessage('description field must be at least 100 characters')
         .matches(/^[a-zA-Z0-9!.,_-]+$/).withMessage('description fiel only must contains letters, numbers, punctuation symbols, and underscore or middle dash'),
     check('price', 'error validating price field')
+        .optional()
         .trim().notEmpty().withMessage('price field cannot be empty')
         .isNumeric().withMessage('price field must be a number')
         .matches(/^[0-9]+$/).withMessage('price field only contains numbers'),
     check('stock', 'error validating stock field')
+        .optional()
         .trim().notEmpty().withMessage('stock field cannot be empty')
         .isNumeric().withMessage('stock field must be a number')
         .matches(/^[0-9]+$/).withMessage('stock field only contains numbers'),
     check('available', 'error validating available field')
+        .optional()
         .isBoolean().withMessage('available field must be true or false'),
     (req, res, next) => {
         fieldValidate(req, res, next);
